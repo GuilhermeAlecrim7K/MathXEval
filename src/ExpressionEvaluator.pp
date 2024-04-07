60c4610adf8b36e4542fc10908e1b4ca47c2b935
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-04-07 17:04:06 -0300

    refactor parsing of number

--- src/ExpressionEvaluator.pp
+++ src/ExpressionEvaluator.pp
@@ -39,6 +39,25 @@ var
   I: integer;
   numberStr: string;
   isFloat: boolean;
+  procedure ParseNumber;
+  begin
+    while (I <= Length(AExpression)) and ((AExpression[I] in ['0'..'9']) or (AExpression[I] = '.')) do
+    begin
+      if
+        ((numberStr = '') and (AExpression[I] = '.'))
+        or (isFloat and (AExpression[I] = '.'))
+      then
+        raise EInvalidToken.Create('Invalid floating point number');
+      isFloat := isFloat or (AExpression[I] = '.');
+      numberStr := numberStr + AExpression[I];
+      Inc(I);
+    end;
+    if isFloat then
+      FSyntaxTree.PushToken(TToken.Create(I, numberStr, FLOAT))
+    else
+      FSyntaxTree.PushToken(TToken.Create(I, numberStr, INT));
+    Dec(I);
+  end;
 begin
   I := 1;
   while I <= Length(AExpression) do
@@ -51,25 +70,7 @@ begin
       '/': FSyntaxTree.PushToken(TToken.Create(I, '/', DIVISION));
       '-': FSyntaxTree.PushToken(TToken.Create(I, '-', SUBTRACTION));
       '+': FSyntaxTree.PushToken(TToken.Create(I, '+', ADDITION));
-      '.', '0'..'9':
-      begin
-        while (I <= Length(AExpression)) and ((AExpression[I] in ['0'..'9']) or (AExpression[I] = '.')) do
-        begin
-          if
-            ((numberStr = '') and (AExpression[I] = '.'))
-            or (isFloat and (AExpression[I] = '.'))
-          then
-            raise EInvalidToken.Create('Invalid floating point number');
-          isFloat := isFloat or (AExpression[I] = '.');
-          numberStr := numberStr + AExpression[I];
-          Inc(I);
-       end;
-        if isFloat then
-          FSyntaxTree.PushToken(TToken.Create(I, numberStr, FLOAT))
-        else
-          FSyntaxTree.PushToken(TToken.Create(I, numberStr, INT));
-        continue;
-      end;
+      '.', '0'..'9': ParseNumber;
     else
       raise EInvalidToken.CreateFmt('Token "%s" invalid at position %d', [AExpression[I], I]);
     end;
